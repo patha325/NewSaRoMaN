e3ab7a78335e4f493d9c321833b0fd5b5a3743d3
Patrik <patrik.hallsjo@gmail.com>
2018-06-08 16:53:35 +0100

    minor changes to analysis

--- analysis/rootScript.C
+++ analysis/rootScript.C
@@ -5,12 +5,12 @@
   T->Add("../recOut.root");
   
   //T->Draw("MCtr_Mom:track_len","","COLZ");
-  TH2D* momentum = new TH2D("momentum",";True Momentum (GeV/c);Rec Momentum (GeV/c)",100,0,4000,100,0,4000);
+  TH2D* momentum = new TH2D("momentum",";True Momentum (GeV/c);Rec Momentum (GeV/c)",100,0,3800,100,0,3800);
 
-  TH2D* momentumDiff = new TH2D("momentumDiff",";True Momentum (GeV/c);Rec-True Momentum (GeV/c)",100,0,4000,100,-5000,5000);
+  TH2D* momentumDiff = new TH2D("momentumDiff",";True Momentum (GeV/c);Rec-True Momentum (GeV/c)",100,0,3800,100,-5000,5000);
 
-  TH1D* eff = new TH1D("eff",";True Momentum (GeV/c); Fractional Efficiency",20, 0, 4000);
-    TH1D* all = new TH1D("all",";True Momentum (GeV/c); Fractional Efficiency",20, 0, 4000);
+  TH1D* eff = new TH1D("eff",";True Momentum (GeV/c); Fractional Efficiency",38, 0, 3800);
+    TH1D* all = new TH1D("all",";True Momentum (GeV/c); Fractional Efficiency",38, 0, 3800);
 
     //T->Draw("abs(mom)-abs(MCtr_Mom):abs(MCtr_Mom)")
     //diff on y, mom on x;
@@ -19,6 +19,7 @@
 
   T->Draw("abs(Rec_mom)-abs(MCtr_Mom):abs(MCtr_Mom)>>momentumDiff","","COLZ");
 
+  // MCtr_Energy
   T->Draw("abs(MCtr_Mom)>>eff","Rec_Charge==-1","COLZ");
   T->Draw("abs(MCtr_Mom)>>all","","COLZ");
   eff->Sumw2();
