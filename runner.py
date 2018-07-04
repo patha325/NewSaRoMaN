c989cd53429da96a3d9cbd25ef2d0fd9e1696576
Patrik <patrik.hallsjo@gmail.com>
2018-07-04 19:25:15 +0100

    Cleaned up the python script:

--- runner.py
+++ runner.py
@@ -8,11 +8,53 @@ import random
 class Runner:
 
     def __init__(self,in_numberOfEvents):
+        self.MCrunNumber = 14 #Number for generating the name of the genie file. Random number.
         self.numberOfEvents = in_numberOfEvents #10000
         self.FNULL = open(os.devnull, 'w')
-        #self.seed = 1000*random.random()
+        self.seed = 1000*random.random()
         self.pdg = 14
+        self.eMin = 0.1 #GeV
+        self.eMax = 4.0 #GeV
+        self.energyRange ='0.1,4.0' #'%s,%s' % (self.eMin,self.eMax)
+        self.NuStorm = True
+        self.fluxfile = "/root/NewSaRoMaN/data/nu_mu_decay_ND.root"
+        self.histoname = "numu_energy_n"
+        self.eventtype = "CCQE"
+        self.scintCode = "1000060120[0.922582],1000010010[0.077418]"
+        self.ironCode = "1000260560"
+        self.scintXSec = "/root/NewSaRoMaN/data/xsec_C12+H1_splines.xml"
+        self.ironXSec = "/root/NewSaRoMaN/data/xsec_Fe56_splines.xml"
 
+        if not self.NuStorm:
+            self.fluxfile = "/root/NewSaRoMaN/data/T2KRHCmodeflux.root"
+            if self.pdg==14:
+                self.histoname = "hist_nu"
+            else:
+                self.histoname = "histo_nubar"
+
+    def runGenie(self,targetCodeStr,targetXSecStr):
+        # https://arxiv.org/abs/1510.05494v1
+        # http://minerva-docdb.fnal.gov/0034/003441/001/GENIE_tutorial-170309.pdf
+        # -n, number of events
+        # -r MC run number
+        # -p PDG code
+        # -t target PDG code
+        # -e neutrino energy range
+        # -f neutrino flux spectrum file
+        # -w Forces generation of weighted events.
+        start = time.time()
+
+
+        command = ['./gevgen','-r',str(self.MCrunNumber),'-n',str(self.numberOfEvents),'-p',str(self.pdg),'-t',targetCodeStr,
+                   '-e',self.energyRange,'-f',self.fluxfile+','+self.histoname,'--seed',str(self.seed),
+                   '--event-generator-list',self.eventtype,'--cross-sections',targetXSecStr,'-w']
+
+        #print subprocess.list2cmdline(command)
+        subprocess.call(subprocess.list2cmdline(command), shell=True, cwd = '/root/genie/bin',stdout=self.FNULL)
+        elapsed = (time.time()-start)
+        print 'Time to run Genie for %s: %s seconds' % (targetCodeStr,elapsed)
+
+        
     def make(self):
         outfile = open("make.log",'w')
         subprocess.call('cmake ..', shell=True, cwd = '/root/NewSaRoMaN/B4c/build',stdout=outfile)
@@ -23,38 +65,11 @@ class Runner:
         
     def run(self):
         #Run genie for scintillator
-        # https://arxiv.org/abs/1510.05494v1
-        # http://minerva-docdb.fnal.gov/0034/003441/001/GENIE_tutorial-170309.pdf
-        # -n, number of events
-        # -r MC run number
-        # -p PDG code
-        # -t target PDG code
-        # -e neutrino energy range
-        # -f neutrino flux spectrum file
-        # -w Forces generation of weighted events.
-        
-        self.seed = 1000*random.random()
-        
-        start = time.time()
-        outfile = open("genie1.log",'w')
-        command = './gevgen -r 14 -n '+str(self.numberOfEvents)+' -p '+str(self.pdg)+' -t 1000060120[0.922582],1000010010[0.077418] -e 0.1,4.0 -f /root/NewSaRoMaN/data/nu_mu_decay_ND.root,numu_energy_n --seed '+str(self.seed)+' --event-generator-list CCQE --cross-sections /root/NewSaRoMaN/data/xsec_C12+H1_splines.xml -w'
-        #command = './gevgen -r 14 -n '+str(self.numberOfEvents)+' -p '+str(self.pdg)+' -t 1000060120[0.922582],1000010010[0.077418] -e 0.01,4.0 -f /root/NewSaRoMaN/data/T2KRHCmodeflux.root,hist_nubar --seed '+str(self.seed)+' --event-generator-list CCQE --cross-sections /root/NewSaRoMaN/data/xsec_C12+H1_splines.xml'
-        subprocess.call(command, shell=True, cwd = '/root/genie/bin',stdout=self.FNULL)#outfile)     
-        outfile.close()
-        subprocess.call('mv gntp.14.ghep.root /root/NewSaRoMaN/B4c/build/genie_active.root ', shell=True, cwd = '/root/genie/bin')
-        elapsed = (time.time()-start)
-        print 'Time to run Genie for Scintillator: %s seconds' % elapsed
-        
+        self.runGenie(self.scintCode,self.scintXSec)
+        subprocess.call('mv gntp.'+str(self.MCrunNumber)+'.ghep.root /root/NewSaRoMaN/B4c/build/genie_active.root ', shell=True, cwd = '/root/genie/bin')
         #Run genie for iron
-        start = time.time()
-        outfile = open("genie2.log",'w')
-        command = './gevgen -r 14 -n '+str(self.numberOfEvents)+' -p '+str(self.pdg)+' -t 1000260560 -e 0.1,4.0 -f /root/NewSaRoMaN/data/nu_mu_decay_ND.root,numu_energy_n --seed '+str(self.seed)+' --event-generator-list CCQE --cross-sections /root/NewSaRoMaN/data/xsec_Fe56_splines.xml -w'
-        #command = './gevgen -r 14 -n '+str(self.numberOfEvents)+' -p '+str(self.pdg)+' -t 1000260560 -e 0.01,4.0 -f /root/NewSaRoMaN/data/T2KRHCmodeflux.root,hist_nubar --seed '+str(self.seed)+' --event-generator-list CCQE --cross-sections /root/NewSaRoMaN/data/xsec_Fe56_splines.xml'
-        subprocess.call(command, shell=True, cwd = '/root/genie/bin',stdout=self.FNULL)#outfile) 
-        outfile.close()
-        subprocess.call('mv gntp.14.ghep.root /root/NewSaRoMaN/B4c/build/genie_passive.root ', shell=True, cwd = '/root/genie/bin')
-        elapsed = (time.time()-start)
-        print 'Time to run Genie for Iron: %s seconds' % elapsed
+        self.runGenie(self.ironCode,self.ironXSec)
+        subprocess.call('mv gntp.'+str(self.MCrunNumber)+'.ghep.root /root/NewSaRoMaN/B4c/build/genie_passive.root ', shell=True, cwd = '/root/genie/bin')
         
         #fix macro file for geant
         
@@ -67,9 +82,9 @@ class Runner:
         start = time.time()
         print 'Starting at %s' % time.ctime()
         subprocess.call('scp WAGASCI.gdml B4c/build', shell=True, cwd = '/root/NewSaRoMaN')
-        outfile = open("geant.log",'w')
+        #outfile = open("geant.log",'w')
         subprocess.call('./exampleB4c -m macro.mac', shell=True, cwd = '/root/NewSaRoMaN/B4c/build',stdout=self.FNULL)#outfile)
-        outfile.close()
+        #outfile.close()
         subprocess.call('mv B4.root /root/NewSaRoMaN/GenFit/build/bin/in.root ', shell=True, cwd = '/root/NewSaRoMaN/B4c/build')
         elapsed = (time.time()-start)
         print 'Time to run Geant: %s seconds' % elapsed
@@ -79,9 +94,9 @@ class Runner:
         start = time.time()
         print 'Starting at %s' % time.ctime()
         subprocess.call('scp WAGASCI.gdml GenFit/build/bin', shell=True, cwd = '/root/NewSaRoMaN')
-        outfile = open("GenFit.log",'w')
+        #outfile = open("GenFit.log",'w')
         subprocess.call('./measurementFactoryExample 0 '+str(self.numberOfEvents), shell=True, cwd = '/root/NewSaRoMaN/GenFit/build/bin',stdout=self.FNULL, stderr=self.FNULL)#outfile)
-        outfile.close()
+        #outfile.close()
         subprocess.call('mv out.root ../../../recOut.root', shell=True, cwd = '/root/NewSaRoMaN/GenFit/build/bin')
         subprocess.call('mv in.root ../../../simOut.root', shell=True, cwd = '/root/NewSaRoMaN/GenFit/build/bin')
         elapsed = (time.time()-start)
